49a9d3e1046c7da19c6faf30d815ca41712f1fd2
hamdane1548 <oussamahamdane80@gmail.com>
2025-10-08 14:29:28 +0100

    version instation dynamique

--- src/main/java/Presentation/Pres2.java
+++ src/main/java/Presentation/Pres2.java
@@ -1,12 +1,25 @@
 package Presentation;
 
 import DAO.DaoImpl2;
+import DAO.IDAO;
+import Metier.IMetier;
 import Metier.ImetierImpl;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
+
 public class Pres2 {
-    public static void main(String[] args) {
-        DaoImpl2  dao = new DaoImpl2();
-        ImetierImpl im = new ImetierImpl(dao);
-        System.out.println("res="+im.Calcul());
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+      Scanner scanner=new Scanner(new File("config.txt"));
+      String daoClassName=scanner.nextLine();
+      Class Cdao=Class.forName(daoClassName);
+      IDAO dao=(IDAO) Cdao.newInstance();
+      String MetierClassName=scanner.nextLine();
+      Class Cmetier=Class.forName(MetierClassName);
+      IMetier metier=(IMetier) Cmetier.getConstructor(IDAO.class).newInstance(dao);
+        System.out.println(metier.Calcul());
+
     }
 }
